f0aceffb90e7f4a11eca08e918eec5e638db5deb
baselomar044-dev <baselomar044@gmail.com>
2026-02-08 00:40:06 +0400

    Add files via upload

---
+++ src/pages/WorkflowPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function WorkflowPage() {
+  return (
+    <div className="flex flex-col h-full p-8">
+      <h1 className="text-3xl font-bold mb-6">Workflow</h1>
+      <div className="bg-gray-100 dark:bg-gray-800 rounded-lg p-6">
+        <p className="text-gray-700 dark:text-gray-300">Workflow page - Coming soon</p>
+      </div>
+    </div>
+  );
+}
